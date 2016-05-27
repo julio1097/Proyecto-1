25920624fc2dd03b4217f77853dad647b29f67ab
julio1097 <julio1097.rojas@gmail.com>
2016-05-27 07:56:31 -0600

    arregle prob 4

--- proyecto1.py
+++ proyecto1.py
@@ -1,1 +1,16 @@
-print ("Hola mundo")
+def prob_1 (n):
+	if n % 2 == 2:
+		return True
+	else:
+		return False
+def prob_2 (n):
+	conver = (n - 32) * 5/9
+	return conver
+def prob_3 (a,b):
+	oper = a ** b
+	return oper
+def prob_4 (num, palabra):
+	cas= (num -len(palabra))//2
+	centrado= num*(cas + len (palabra))
+	return "*"*cas+palabra+"*"cas2
+
---
+++ prueba.py
@@ -0,0 +1,5 @@
+from proyecto1 import prob_4
+palabra= str(input("Ingrese la palabra a centrar: "))
+num= int(input("Ingrese numero para centrar palabra: "))
+calculo = prob_4 (num, palabra)
+print (calculo)
